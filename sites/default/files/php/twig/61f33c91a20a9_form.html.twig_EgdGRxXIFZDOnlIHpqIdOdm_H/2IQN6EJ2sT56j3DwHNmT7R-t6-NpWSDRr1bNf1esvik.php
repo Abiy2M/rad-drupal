8e39db9d18c981e74b79c12afc77acba90c9f6cd
AbiyM1 <it.abiy.menberu19@gmail.com>
2022-01-28 08:05:35 +0300

    added 10 blogs and added a bootstrap UI

---
+++ sites/default/files/php/twig/61f33c91a20a9_form.html.twig_EgdGRxXIFZDOnlIHpqIdOdm_H/2IQN6EJ2sT56j3DwHNmT7R-t6-NpWSDRr1bNf1esvik.php
@@ -0,0 +1,97 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/fashion_zymphonies_theme/templates/form/form.html.twig */
+class __TwigTemplate_64be106062a616520aa7a63d4f54f34d8195e3a4faaa0bfc53b0f2e5b1844a65 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 13
+        echo "<form";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 13, $this->source), "html", null, true);
+        echo ">
+  ";
+        // line 14
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null), 14, $this->source), "html", null, true);
+        echo "
+</form>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/fashion_zymphonies_theme/templates/form/form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  44 => 14,  39 => 13,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/fashion_zymphonies_theme/templates/form/form.html.twig", "C:\\xampp\\htdocs\\drupal-gaze\\themes\\fashion_zymphonies_theme\\templates\\form\\form.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 13);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
